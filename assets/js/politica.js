22ec6e7d57887eabd9bb2334c87497b9526284cc
Itmt64 <150291687+Itmt64@users.noreply.github.com>
2023-11-17 14:42:36 +0100

    Add files via upload

---
+++ assets/js/politica.js
@@ -0,0 +1,6 @@
+var resultado = window.confirm('Utilizamos cookies propias y de terceros para mejorar nuestros servicios y mostrarle publicidad relacionada con sus preferencias mediante el análisis de sus hábitos de navegación. Si continúa navegando, consideramos que acepta su uso.?');
+if (resultado === true) {
+    window.alert('Okay, si estas seguro.');
+} else { 
+    window.alert('Pareces indeciso');
+}
