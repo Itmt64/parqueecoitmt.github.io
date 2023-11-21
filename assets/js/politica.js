0bd366a0d07dc0195cae7fba9ba370ed94a82af7
Itmt64 <150291687+Itmt64@users.noreply.github.com>
2023-11-21 09:37:07 -0400

    Update politica.js

--- assets/js/politica.js
+++ assets/js/politica.js
@@ -1,4 +1,4 @@
-var resultado = window.confirm('Utilizamos cookies propias y de terceros para mejorar nuestros servicios, haciendo uso de su información personal para el caso de adquirlo mediante el qr del sitio. Si continúa navegando, consideramos que acepta su uso.?');
+var resultado = window.confirm('Utilizamos cookies propias y de terceros para mejorar nuestros servicios, haciendo uso de su información personal para el caso de adquir el ticket de ingreso al parque mediante el qr del sitio. Si continúa navegando, consideramos que acepta su uso.?');
 if (resultado === true) {
     window.alert('Okay, si estas seguro.');
 } else { 
